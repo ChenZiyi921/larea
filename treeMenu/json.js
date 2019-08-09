220a9ae7e7ab6ad1a876658b78e8d3ffc9606ddb
ChenZiyi921 <919105896@qq.com>
2019-08-09 09:58:38 +0800

    treeMenu

---
+++ treeMenu/json.js
@@ -0,0 +1,159 @@
+var result = {
+    "code": 200,
+    "message": "OK",
+    "data": [
+        {
+            "id": 130,
+            "createUser": "dd",
+            "updateUser": "dd",
+            "createTime": "2019-07-15 16:47:15",
+            "updateTime": "2019-07-15 16:47:15",
+            "customerId": null,
+            "platform": 1,
+            "parentId": 0,
+            "menuName": "页面",
+            "url": null,
+            "description": "",
+            "level": 0,
+            "children": [
+                {
+                    "id": 131,
+                    "createUser": "dd",
+                    "updateUser": "dd",
+                    "createTime": "2019-07-15 16:47:32",
+                    "updateTime": "2019-07-15 16:47:32",
+                    "customerId": null,
+                    "platform": 1,
+                    "parentId": 130,
+                    "menuName": "商城管理",
+                    "url": null,
+                    "description": "",
+                    "level": 1,
+                    "children": null
+                },
+                {
+                    "id": 132,
+                    "createUser": "dd",
+                    "updateUser": "dd",
+                    "createTime": "2019-07-15 16:47:44",
+                    "updateTime": "2019-07-15 16:47:44",
+                    "customerId": null,
+                    "platform": 1,
+                    "parentId": 130,
+                    "menuName": "页面管理",
+                    "url": null,
+                    "description": "",
+                    "level": 1,
+                    "children": [
+                        {
+                            "id": 133,
+                            "createUser": "dd",
+                            "updateUser": "dd",
+                            "createTime": "2019-07-15 16:48:08",
+                            "updateTime": "2019-07-15 16:48:08",
+                            "customerId": null,
+                            "platform": 1,
+                            "parentId": 132,
+                            "menuName": "添加页面",
+                            "url": null,
+                            "description": "",
+                            "level": 2,
+                            "children": null
+                        },
+                        {
+                            "id": 134,
+                            "createUser": "dd",
+                            "updateUser": "dd",
+                            "createTime": "2019-07-15 16:48:32",
+                            "updateTime": "2019-07-15 16:48:32",
+                            "customerId": null,
+                            "platform": 1,
+                            "parentId": 132,
+                            "menuName": "添加分类",
+                            "url": null,
+                            "description": "",
+                            "level": 2,
+                            "children": null
+                        },
+                        {
+                            "id": 135,
+                            "createUser": "dd",
+                            "updateUser": "dd",
+                            "createTime": "2019-07-15 16:49:18",
+                            "updateTime": "2019-07-15 16:49:18",
+                            "customerId": null,
+                            "platform": 1,
+                            "parentId": 132,
+                            "menuName": "页面列表",
+                            "url": null,
+                            "description": "",
+                            "level": 2,
+                            "children": null
+                        }
+                    ]
+                },
+                {
+                    "id": 136,
+                    "createUser": "dd",
+                    "updateUser": "dd",
+                    "createTime": "2019-07-15 16:49:44",
+                    "updateTime": "2019-07-15 16:49:44",
+                    "customerId": null,
+                    "platform": 1,
+                    "parentId": 130,
+                    "menuName": "素材库",
+                    "url": null,
+                    "description": "",
+                    "level": 1,
+                    "children": null
+                }
+            ]
+        },
+        {
+            "id": 138,
+            "createUser": "dd",
+            "updateUser": "dd",
+            "createTime": "2019-07-15 16:54:37",
+            "updateTime": "2019-07-15 16:54:37",
+            "customerId": null,
+            "platform": 1,
+            "parentId": 0,
+            "menuName": "商品",
+            "url": null,
+            "description": "",
+            "level": 0,
+            "children": [
+                {
+                    "id": 139,
+                    "createUser": "dd",
+                    "updateUser": "dd",
+                    "createTime": "2019-07-15 16:55:19",
+                    "updateTime": "2019-07-15 16:55:19",
+                    "customerId": null,
+                    "platform": 1,
+                    "parentId": 138,
+                    "menuName": "商品管理",
+                    "url": null,
+                    "description": "",
+                    "level": 1,
+                    "children": null
+                },
+                {
+                    "id": 140,
+                    "createUser": "dd",
+                    "updateUser": "dd",
+                    "createTime": "2019-07-15 16:55:33",
+                    "updateTime": "2019-07-15 16:55:33",
+                    "customerId": null,
+                    "platform": 1,
+                    "parentId": 138,
+                    "menuName": "售后设置",
+                    "url": null,
+                    "description": "",
+                    "level": 1,
+                    "children": null
+                }
+            ]
+        }
+    ]
+}
